17c13d00edfefdc7b0d9ea6903e57bf135e9a8bf
Donatas Rasiukevicius <d.rasiukevicius@uu.nl>
2023-03-20 15:19:30 +0100

    Add filtering logic

--- humitifier/app.py
+++ humitifier/app.py
@@ -6,6 +6,7 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from humitifier.models import Server
 
+from typing import Optional
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
@@ -30,6 +31,12 @@ def index(request: Request):
             "title": "All backoffice servers",
             "display_count": len(servers),
             "critical_count": len([s for s in servers.values() if s.status == "critical"]),
+            "list_filters": {
+                "hostname": [s for s in servers],
+                "os": set([s.os for s in servers.values()]),
+                "department": set([s.requesting_department for s in servers.values()]),
+                "server_type": set([s.server_type for s in servers.values()]),
+            },
         },
     )
 
@@ -38,4 +45,33 @@ def index(request: Request):
 def reload_server_details(request: Request, server_name: str):
     return templates.TemplateResponse(
         "htmx/server-details.jinja", {"request": request, "server": servers[server_name]}
+    )
+
+
+@app.get("/htmx-filter-grid-by")
+def filter_server_grid(
+    request: Request,
+    hostname: Optional[str] = None,
+    os: Optional[str] = None,
+    department: Optional[str] = None,
+    server_type: Optional[str] = None,
+):
+    filtered = servers.values()
+    if hostname:
+        filtered = [f for f in filtered if hostname in f.name]
+    if os:
+        filtered = [f for f in filtered if os == f.os]
+    if department:
+        filtered = [f for f in filtered if department in f.requesting_department]
+    if server_type:
+        filtered = [f for f in filtered if server_type in f.server_type]
+    return templates.TemplateResponse(
+        "htmx/filtered-server-grid.jinja",
+        {
+            "request": request,
+            "servers": filtered,
+            "title": "Filtered view",
+            "display_count": len(filtered),
+            "critical_count": len([s for s in filtered if s.status == "critical"]),
+        },
     )
