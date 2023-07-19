7419b28d67246adb5c2c844adea04fb1c2aa3da5
Donatas Rasiukevičius <d.rasiukevicius@uu.nl>
2023-07-19 14:20:09 +0200

    Rework filters to enum

--- humitifier/filters.py
+++ humitifier/filters.py
@@ -1,122 +1,103 @@
-from humitifier.models.host import Host
-from humitifier.models.factping import FactPing
+"""Filters act as a way to filter the hosts based on a specific criteria.
+The filters can be extend so long as they provide the same interface as the currently implemented filter, meaning they have:
+- a `slug` property that returns a string
+- a `label` property that returns a string
+- a `widget` property that returns a string
+- an `options` method that returns a list of strings
+- an `apply` method that takes a list of HostState and query and returns a bool
+- a `from_key` method that takes a string and returns a Filter
+"""
+
+from enum import Enum, auto
+
+from humitifier.models.host_state import HostState
 from humitifier.utils import partial_match, flatten_list
 
-from typing import Literal, Protocol
+from typing import Literal
 
 
-class Filter(Protocol):
-    slug: str
-    name: str
-    widget: Literal["select", "search"]
+class Filter(Enum):
+    Hostname = auto()
+    Os = auto()
+    Package = auto()
+    Department = auto()
+    Owner = auto()
+    Purpose = auto()
+    Person = auto()
+
+    @property
+    def widget(self) -> Literal["search", "select"]:
+        match self:
+            case Filter.Hostname | Filter.Package | Filter.Owner | Filter.Person:
+                return "search"
+            case Filter.Os | Filter.Department | Filter.Purpose:
+                return "select"
+            
+    @property
+    def label(self) -> str:
+        return self.name.capitalize()
+    
+    @property
+    def slug(self) -> str:
+        return self.name.lower()
+            
+
+    def options(self, hosts: list[HostState]) -> list[str]:
+        match self:
+            case Filter.Hostname:
+                options = [h.facts.hostname for h in hosts if h.facts.hostname]
+            case Filter.Os:
+                options = [h.facts.os for h in hosts if h.facts.os]
+            case Filter.Package:
+                packages = flatten_list([h.facts.packages for h in hosts])
+                options = [p.name for p in packages]
+            case Filter.Department:
+                options = [h.host.metadata["department"] for h in hosts if h.host.metadata["department"]]
+            case Filter.Owner:
+                options = [h.host.metadata["owner"]["name"] for h in hosts if h.host.metadata["owner"]["name"]]
+            case Filter.Purpose:
+                options = [h.host.metadata["purpose"] for h in hosts if h.host.metadata["purpose"]]
+            case Filter.Person:
+                person_lists = [h.host.metadata["people"] for h in hosts if h.host.metadata and h.host.metadata.get("people")]
+                all_people = flatten_list(person_lists)
+                options = [p["name"] for p in all_people]
+        return list(sorted(set(options)))
 
 
-    def __call__(self, host: Host, host_facts: FactPing, query: str) -> bool: ...
+    def apply(self, host_state: HostState, query: str) -> bool:
+        match self:
+            case Filter.Hostname:
+                return host_state.facts.hostname and query in host_state.facts.hostname
+            case Filter.Os:
+                return host_state.facts.os and query in host_state.facts.os
+            case Filter.Package:
+                return host_state.facts.packages and partial_match([p.name for p in host_state.facts.packages], query)
+            case Filter.Department:
+                if not host_state.host.metadata or not host_state.host.metadata["department"]:
+                    return False
+                return query in host_state.host.metadata["department"]
+            case Filter.Owner:
+                if not host_state.host.metadata or not getattr(host_state.host.metadata, "owner", None):
+                    return False
+                return query in host_state.host.metadata["owner"]["name"]
+            case Filter.Purpose:
+                if not host_state.host.metadata or not host_state.host.metadata.get("purpose"):
+                    return False
+                return query in host_state.host.metadata["purpose"]
+            case Filter.Person:
+                if not host_state.host.metadata or not host_state.host.metadata.get("people"):
+                    return False
+                return partial_match([p["name"] for p in host_state.host.metadata["people"]], query)
 
-    @staticmethod
-    def options(hosts: list[tuple[Host, FactPing]]) -> list[str]: ...
-
-class HostFilter:
-    slug = "hostname"
-    name = "Hostname"
-    widget = "search"
-
-    def __call__(self, host: Host, host_facts: FactPing, query: str) -> bool:
-        return host_facts.hostname and query in host_facts.hostname
+    @classmethod
+    def from_key(cls, key: str) -> "Filter":
+        return cls[key.capitalize()]
+    
     
     @staticmethod
-    def options(hosts: list[tuple[Host, FactPing]]) -> list[str]:
-        return [host_facts.hostname for _, host_facts in hosts if host_facts.hostname]
-    
-
-class OsFilter:
-    slug = "os"
-    name = "Operating System"
-    widget = "select"
-
-    def __call__(self, host: Host, host_facts: FactPing, query: str) -> bool:
-        return host_facts.os and query in host_facts.os
-    
-    @staticmethod
-    def options(hosts: list[tuple[Host, FactPing]]) -> list[str]:
-        return list(set([host_facts.os for _, host_facts in hosts if host_facts.os]))
-    
-
-class PackageFilter:
-    slug = "package"
-    name = "Package"
-    widget = "search"
-
-    def __call__(self, host: Host, host_facts: FactPing, query: str) -> bool:
-        return host_facts.packages and partial_match([p.name for p in host_facts.packages], query)
-    
-    @staticmethod
-    def options(hosts: list[tuple[Host, FactPing]]) -> list[str]:
-        package_lists = [host_facts.packages for _, host_facts in hosts if host_facts.packages]
-        all_packages = flatten_list(package_lists)
-        return list(set([p.name for p in all_packages]))
-    
-
-class DepartmentFilter:
-    slug = "department"
-    name = "Department"
-    widget = "select"
-
-    def __call__(self, host: Host, host_facts: FactPing, query: str) -> bool:
-        if not host.metadata or not host.metadata["department"]:
-            return False
-        return query in host.metadata["department"]
-    
-    @staticmethod
-    def options(hosts: list[tuple[Host, FactPing]]) -> list[str]:
-        departments = [host.metadata["department"] for host, _ in hosts if host.metadata and host.metadata.get("department")]
-        return list(set(departments))
-    
-
-class OwnerFilter:
-    slug = "owner"
-    name = "Owner"
-    widget = "search"
-
-    def __call__(self, host: Host, host_facts: FactPing, query: str) -> bool:
-        if not host.metadata or not getattr(host.metadata, "owner", None):
-            return False
-        return query in host.metadata["owner"]["name"]
-    
-    @staticmethod
-    def options(hosts: list[tuple[Host, FactPing]]) -> list[str]:
-        owners = [host.metadata["owner"]["name"] for host, _ in hosts if host.metadata and host.metadata["owner"]]
-        return list(set(owners))
-    
-
-class PurposeFilter:
-    slug = "purpose"
-    name = "Purpose"
-    widget = "select"
-
-    def __call__(self, host: Host, host_facts: FactPing, query: str) -> bool:
-        if not host.metadata or not host.metadata.get("purpose"):
-            return False
-        return query in host.metadata["purpose"]
-    
-    @staticmethod
-    def options(hosts: list[tuple[Host, FactPing]]) -> list[str]:
-        purposes = [host.metadata["purpose"] for host, _ in hosts if host.metadata and host.metadata.get("purpose")]
-        return list(set(purposes))
-    
-
-class PersonFilter:
-    slug = "person"
-    name = "Person"
-    widget = "search"
-
-    def __call__(self, host: Host, host_facts: FactPing, query: str) -> bool:
-        if not host.metadata or not host.metadata.get("people"):
-            return False
-        return partial_match([p["name"] for p in host.metadata["people"]], query)
-    
-    @staticmethod
-    def options(hosts: list[tuple[Host, FactPing]]) -> list[str]:
-        person_lists = [host.metadata["people"] for host, _ in hosts if host.metadata and host.metadata.get("people")]
-        all_people = flatten_list(person_lists)
-        return list(set([p["name"] for p in all_people]))
+    def apply_from_kv(host_states: list[HostState], filter_kv: dict[str, str]) -> list[HostState]:
+        filtered = host_states
+        for filter_key, query in filter_kv.items():
+            filter = Filter.from_key(filter_key)
+            filtered = [h for h in filtered if filter.apply(h, query)]
+        return filtered
