1769d69dfb504cf85aa74d8efe0c14f3e04b49f0
Donatas Rasiukevičius <d.rasiukevicius@uu.nl>
2023-08-10 12:19:00 +0200

    Re-implement app initialization

--- humitifier/app.py
+++ humitifier/app.py
@@ -1,67 +1,57 @@
 from fastapi import APIRouter, FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
-from pssh.clients import ParallelSSHClient
-from humitifier.config import AppConfig
-from humitifier.filters import apply_from_query_params
-from humitifier.views.components.host_grid import HostGrid
-from humitifier.views.components.host_modal import HostModal
-from humitifier.views.pages.index import HostGridIndex
-from humitifier.views.utils import Wrapper
+from humitifier.state.app import AppState, HostCollectionState
+from humitifier.html import HostGrid, Page, FilterSet, HxSwap, HostModal, HtmlString, HostGridItems
+from humitifier.urls import Url
+
 
 
 router = APIRouter()
 
+def _get_state(request: Request) -> AppState:
+    return request.app.state.app_state
 
-@router.get("/", response_class=HTMLResponse)
+
+@router.get(Url.Index.value, response_class=HTMLResponse)
 async def index(request: Request):
-    host_grid = HostGridIndex.create(
-        host_states=list(request.app.state.host_states_kv.values()),
-        app_config=request.app.state.config
-    )
-    return HTMLResponse(
-            Wrapper.Base.render(html_content=host_grid.html)
+    state = _get_state(request)
+    if request.headers.get("HX-Request"):
+        all_hosts = state.host_collection.values()
+        hosts = state.filterset.apply(
+            {k:v for k, v in request.query_params.items() if v},
+            all_hosts
         )
+        collection = HostCollectionState({h.fqdn: h for h in hosts})
+        swap = HxSwap(component=collection.component(HostGridItems), target="innerHTML:.grid")
+        return HTMLResponse(swap.render())
+    page = Page([
+        state.filterset.component(FilterSet),
+        state.host_collection.component(HostGrid)
+    ])
+    return HTMLResponse(page.render())
 
 
-@router.get("/hx-host-modal/{fqdn}")
+@router.get(Url.HostDetails.value)
 async def host_details(request: Request, fqdn: str):
-    host_state = request.app.state.host_states_kv[fqdn]
+    state = _get_state(request)
+    host_state = state.host_collection[fqdn]
     target = "innerHTML:#hx-modal"
-    modal = HostModal.create(
-        host_state=host_state,
-        metadata_properties=request.app.state.config.metadata_properties,
-        fact_properties=request.app.state.config.fact_properties
-    )
-    return HTMLResponse(
-        Wrapper.HxOobSwap.render(html_content=modal.html, target=target)
-    )
+    modal = host_state.component(HostModal)
+    swap = HxSwap(component=modal, target=target)
+    return HTMLResponse(swap.render())
 
 
-@router.get("/hx-filter-hosts")
-async def filter_hosts(request: Request):
-    params = {k: v for k, v in request.query_params.items() if v}
-    host_states = list(request.app.state.host_states_kv.values())
-    filtered_hosts = apply_from_query_params(host_states, query_params=params, filter_kv=request.app.state.config.filter_kv)
-    hostgrid = HostGrid.create(host_states=filtered_hosts, grid_properties=request.app.state.config.grid_properties)
-    return HTMLResponse(
-        Wrapper.HxOobSwap.render(html_content=hostgrid.inner_html, target="innerHTML:.grid")
-    )
 
-
-@router.get("/hx-clear-host-modal")
+@router.get(Url.CloseModal.value)
 async def clear_target(request: Request):
-    return HTMLResponse(
-        Wrapper.HxOobSwap.render(html_content="", target="innerHTML:#hx-modal")
-    )
+    swap = HxSwap(component=HtmlString(""), target="innerHTML:#hx-modal")
+    return HTMLResponse(swap.render())
 
 
-def create_base_app(config: AppConfig) -> FastAPI:
+def create_base_app() -> FastAPI:
     app = FastAPI()
     app.mount("/static", StaticFiles(directory="static"), name="static")
-    app.state.config = config
-    app.state.pssh_client = ParallelSSHClient([h.fqdn for h in config.hosts], **config.pssh_conf)
-    app.state.host_states_kv = {}
     app.include_router(router)
     return app
 
