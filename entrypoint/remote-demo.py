169b0891229097608b7fbc9daf721e0db895176c
Donatas Rasiukevičius <d.rasiukevicius@uu.nl>
2023-10-05 08:41:50 +0200

    Remove task definition

--- entrypoint/remote-demo.py
+++ entrypoint/remote-demo.py
@@ -1,4 +1,3 @@
-
 import uvicorn
 import asyncio
 from humitifier.facts import HostnameCtl, Blocks, Groups, Memory, PackageList, Uptime, Users
@@ -7,31 +6,21 @@ from humitifier.state.app import AppState
 
 from humitifier.props import (
     Fqdn,
-    Department, 
+    Department,
     Hostname,
-    EndDate,
     LocalStorage,
-    MemoryUse
+    MemoryUse,
+    Os,
+    PackageList,
+    Virtualization,
 )
 from humitifier.config.host_view import HostViewConfig
 from humitifier.app import router, FastAPI, StaticFiles
 from humitifier.html import SelectInput, SearchInput
 
 DEFAULT_VIEW = HostViewConfig(
-    card=[
-        Hostname,
-        Department,
-        # EndDate,
-        LocalStorage,
-        MemoryUse
-
-    ],
-    table=[
-        Hostname,
-        Department,
-        LocalStorage,
-        MemoryUse
-    ],
+    card=[Hostname, Department, LocalStorage, MemoryUse],
+    table=[Hostname, Os, Department, LocalStorage, MemoryUse, Virtualization, PackageList],
 )
 
 
@@ -39,21 +28,17 @@ config = AppConfig(
     hosts=[
         HostConfig(
             fqdn=Fqdn("gw-c-dh-static.im.hum.uu.nl"),
-            metadata=[
-                Department("dev"),
-            ],
-            view_cfg=DEFAULT_VIEW
+            metadata=[],
+            view_cfg=DEFAULT_VIEW,
         ),
         HostConfig(
             fqdn=Fqdn("gw-d11-fsw-davinci-acc.im.hum.uu.nl"),
-            metadata=[
-                # Department("not-dev"),
-            ],
-            view_cfg=DEFAULT_VIEW
-        )
+            metadata=[],
+            view_cfg=DEFAULT_VIEW,
+        ),
     ],
     facts=FactConfig(
-        { 
+        {
             HostnameCtl,
             Blocks,
             Groups,
@@ -63,15 +48,14 @@ config = AppConfig(
             Users,
         }
     ),
-    filters=FiltersetConfig([
-        (Department, SelectInput),
-        (Hostname, SearchInput)
-    ]),
+    filters=FiltersetConfig(
+        [(Department, SelectInput), (Hostname, SearchInput), (Os, SelectInput), (PackageList, SearchInput)]
+    ),
     pssh_conf={
         "user": "donatas",
         "proxy_host": "cratylus.im.hum.uu.nl",
     },
-    poll_interval="every 10 minutes"
+    poll_interval="every 10 minutes",
 )
 
 
@@ -81,14 +65,17 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 app.state.app_state = state
 app.include_router(router)
 
+
 class Server(uvicorn.Server):
     """Customized uvicorn.Server
 
     Uvicorn server overrides signals and we need to include
     Rocketry to the signals."""
+
     # def handle_exit(self, sig: int, frame) -> None:
     #     task_app.session.shut_down()
     #     return super().handle_exit(sig, frame)
+
 
 async def main():
     "Run scheduler and the API"
@@ -98,6 +85,7 @@ async def main():
     # sched = asyncio.create_task(task_app.serve())
 
     await asyncio.wait([dash])
+
 
 if __name__ == "__main__":
     asyncio.run(main())
