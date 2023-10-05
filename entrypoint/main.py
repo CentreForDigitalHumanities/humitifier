af031b98d5d8e12c018f32e9d4f7d52569f904a8
Donatas Rasiukevičius <d.rasiukevicius@uu.nl>
2023-10-05 08:53:10 +0200

    Add app building to entrypoint

--- entrypoint/main.py
+++ entrypoint/main.py
@@ -1,10 +1,13 @@
 import uvicorn
 import asyncio
-from user_config import app  # noqa
-from humitifier.tasks import create_scheduler
+from humitifier.state.app import AppState
+from humitifier.app import router, FastAPI, StaticFiles
+from user_config import config  # noqa
+
+# from humitifier.tasks import create_scheduler
 
 
-task_app = create_scheduler(app)
+# task_app = create_scheduler(app)
 
 
 # class Server(uvicorn.Server):
@@ -15,6 +18,13 @@ task_app = create_scheduler(app)
 #     def handle_exit(self, sig: int, frame) -> None:
 #         task_app.session.shut_down()
 #         return super().handle_exit(sig, frame)
+
+
+state = AppState.initialize(config)
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+app.state.app_state = state
+app.include_router(router)
 
 
 async def main():
