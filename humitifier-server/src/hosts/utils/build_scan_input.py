31bed390bb2b3f2ea3fa12fac4ada17f4eda1d1b
Mees, T.D. (Ty) <t.d.mees@uu.nl>
2025-01-13 10:39:54 +0100

    refactor: rename facts to artefacts in scan input logic

--- humitifier-server/src/hosts/utils/build_scan_input.py
+++ humitifier-server/src/hosts/utils/build_scan_input.py
@@ -1,6 +1,6 @@
 from hosts.models import Host
-from humitifier_common.scan_data import FactScanOptions, ScanInput
-from humitifier_common.facts import registry as facts_registry
+from humitifier_common.scan_data import ArtefactScanOptions, ScanInput
+from humitifier_common.artefacts import registry as facts_registry
 
 
 def build_scan_input(hostname: str | Host) -> ScanInput:
@@ -10,8 +10,8 @@ def build_scan_input(hostname: str | Host) -> ScanInput:
 
     # TODO: proper scan input building
     all_facts = facts_registry.all()
-    all_facts = [fact.__fact_name__ for fact in all_facts]
+    all_facts = [fact.__artefact_name__ for fact in all_facts]
 
-    requested_facts = {fact: FactScanOptions() for fact in all_facts}
+    requested_facts = {fact: ArtefactScanOptions() for fact in all_facts}
 
     return ScanInput(hostname=host.fqdn, facts=requested_facts)
