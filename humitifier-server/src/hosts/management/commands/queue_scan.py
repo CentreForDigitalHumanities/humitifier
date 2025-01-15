e2f1f98eb2348175887ebc5ad21e80108e452f3f
Mees, T.D. (Ty) <t.d.mees@uu.nl>
2025-01-15 15:09:40 +0100

    `feat: add --force option to override scan scheduling checks`
    Enhanced the `queue_scan` command with a `--force` option to bypass scan scheduling restrictions. This ensures scans can be queued manually even when restrictions are active, provided `settings.DEBUG` is disabled.

--- humitifier-server/src/hosts/management/commands/queue_scan.py
+++ humitifier-server/src/hosts/management/commands/queue_scan.py
@@ -1,12 +1,14 @@
 from django.core.management.base import BaseCommand, CommandError
 
 from hosts.models import Host
+from humitifier_server import settings
 
 
 class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument("host")
+        parser.add_argument("--force", action="store_true")
 
     def handle(self, *args, **options):
         from hosts.tasks import start_scan
@@ -14,8 +16,9 @@ class Command(BaseCommand):
         host = Host.objects.get(fqdn=options["host"])
 
         if not host.can_schedule_scan:
-            raise CommandError(
-                "Cannot queue scans for hosts using non-scheduled scheduling"
-            )
+            if not settings.DEBUG and not options["force"]:
+                raise CommandError(
+                    "Cannot queue scans for hosts using non-scheduled scheduling"
+                )
 
         start_scan.delay(host.fqdn)
