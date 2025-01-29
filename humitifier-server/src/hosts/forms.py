2ca34532affd477b42d33a9645aa632adefc379e
Mees, T.D. (Ty) <t.d.mees@uu.nl>
2025-01-29 14:12:26 +0100

    `fix: ensure identifier field handles auto-generated values`
    Refactored the `identifier` field to be optional and handle cases where it relies on auto-generated values. Added logic to clean the `identifier` field by checking for a provided initial value in `self.data`. This ensures robust handling of the field without relying on user input.

--- humitifier-server/src/hosts/forms.py
+++ humitifier-server/src/hosts/forms.py
@@ -11,3 +11,21 @@ class DataSourceForm(forms.ModelForm):
             "source_type": forms.RadioSelect,
             "identifier": forms.TextInput(attrs={"disabled": True}),
         }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # It's a disabled field, so it won't be sent to the server on submission
+        # We're relying on auto-generated values anyway
+        self.fields["identifier"].required = False
+
+    def clean_identifier(self):
+        # Should not happen?
+        if "identifier" in self.cleaned_data and self.cleaned_data["identifier"]:
+            return self.cleaned_data["identifier"]
+
+        # Get the initial auto-generated value from data
+        if "initial-identifier" in self.data:
+            return self.data["initial-identifier"]
+
+        return None
