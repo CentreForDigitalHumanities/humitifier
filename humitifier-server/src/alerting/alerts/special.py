68628c3d2a4fca25d99cc0311a4c4cf78d095734
Mees, T.D. (Ty) <t.d.mees@uu.nl>
2025-03-19 14:19:15 +0100

    `feat(alerting): add ZFS usage alert generator`
    Introduces `ZFSUsageAlertGenerator` to monitor ZFS pool and volume usage. Alerts are generated when usage exceeds defined warning (80%) or critical (90%) thresholds, ensuring proactive notification.

---
+++ humitifier-server/src/alerting/alerts/special.py
@@ -0,0 +1,50 @@
+from typing import Literal
+
+from alerting.backend.data import AlertData
+from alerting.backend.generator import BaseArtefactAlertGenerator
+from alerting.models import AlertSeverity
+from humitifier_common.artefacts import ZFS, ZFSPool, ZFSVolume
+
+
+class ZFSUsageAlertGenerator(BaseArtefactAlertGenerator):
+    artefact = ZFS
+    verbose_name = "ZFS Usage"
+
+    HIGH_USAGE_THRESHOLD = 90  # percentage
+    WARNING_USAGE_THRESHOLD = 80  # percentage
+
+    def generate_alerts(self) -> AlertData | list[AlertData] | None:
+        if not self.artefact_data:
+            return None
+
+        zfs: ZFS = self.artefact_data
+
+        return self.process_items(zfs.pools, "pool") + self.process_items(
+            zfs.volumes, "volume"
+        )
+
+    def process_items(
+        self,
+        items: list[ZFSPool] | list[ZFSVolume],
+        item_type: Literal["pool", "volume"],
+    ) -> list[AlertData]:
+        alerts = []
+        for item in items:
+            percent_usage = item.used_mb / item.size_mb * 100
+            if percent_usage > self.HIGH_USAGE_THRESHOLD:
+                alerts.append(
+                    AlertData(
+                        severity=AlertSeverity.CRITICAL,
+                        message=f"ZFS {item_type} '{item.name}' usage exceeds {self.HIGH_USAGE_THRESHOLD}%",
+                        custom_identifier=f"{item_type}-{item.name}",
+                    )
+                )
+            elif percent_usage > self.WARNING_USAGE_THRESHOLD:
+                alerts.append(
+                    AlertData(
+                        severity=AlertSeverity.WARNING,
+                        message=f"ZFS {item_type} '{item.name}' usage exceeds {self.WARNING_USAGE_THRESHOLD}%",
+                        custom_identifier=f"{item_type}-{item.name}",
+                    )
+                )
+        return alerts
