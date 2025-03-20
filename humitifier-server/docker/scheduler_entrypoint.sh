a8534ef727dbab195458c303f43f50ef2d0813a2
Mees, T.D. (Ty) <t.d.mees@uu.nl>
2025-03-20 11:53:51 +0100

    fix: set proper scheduler

--- humitifier-server/docker/scheduler_entrypoint.sh
+++ humitifier-server/docker/scheduler_entrypoint.sh
@@ -7,4 +7,4 @@ source /app/.venv/bin/activate
 LOG_LEVEL=${LOG_LEVEL:-INFO}
 
 # Run da scheduler
-exec python -m celery -A humitifier_server beat -l "$LOG_LEVEL"
+exec python -m celery -A humitifier_server beat -l "$LOG_LEVEL" --scheduler django_celery_beat.schedulers:DatabaseScheduler
