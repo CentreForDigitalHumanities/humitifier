1e787f78f0bc80309c372deeeefaa826aafa5f00
Mees, T.D. (Ty) <t.d.mees@uu.nl>
2025-03-20 15:08:23 +0100

    feat: make log level configurable via environment variable
    The `LOG_LEVEL` can now be set through an environment variable, allowing for better configurability. If not specified, it defaults to `INFO` to ensure consistent behavior.

--- humitifier-scanner/docker/entrypoint.sh
+++ humitifier-scanner/docker/entrypoint.sh
@@ -6,5 +6,8 @@ source /app/.venv/bin/activate
 # CD into the app directory
 cd /app/src/ || exit 1
 
+# Set default log level to INFO if not provided via the environment variable
+LOG_LEVEL=${LOG_LEVEL:-INFO}
+
 # Run celery worker
-exec python -m celery -A humitifier_scanner.celery_worker worker -Q scanner -l INFO -n scanner@%h
+exec python -m celery -A humitifier_scanner.celery_worker worker -Q scanner -l "$LOG_LEVEL" -n scanner@%h
