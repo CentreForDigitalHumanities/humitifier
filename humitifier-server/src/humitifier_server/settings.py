bee4db1261e39d019f35bebc7461d24be8fd6dbd
Mees, T.D. (Ty) <t.d.mees@uu.nl>
2025-04-14 13:21:42 +0200

    chore: bump project versions to 4.3.1
    Updated version numbers across `humitifier-common`, `humitifier-server`, and `humitifier-scanner` to 4.3.1. This change maintains consistency in project metadata and reflects the latest updates.

--- humitifier-server/src/humitifier_server/settings.py
+++ humitifier-server/src/humitifier_server/settings.py
@@ -23,7 +23,7 @@ from . import env
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-HUMITIFIER_VERSION = "4.3.0"
+HUMITIFIER_VERSION = "4.3.1"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
