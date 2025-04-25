5272db68c1bac4cded4d1b962792193e61d01119
Mees, T.D. (Ty) <t.d.mees@uu.nl>
2025-04-25 16:51:22 +0200

    fix: exclude acknowledged alerts from stats calculations
    Previously, the alert statistics and counts included alerts that were already acknowledged, which could lead to misleading data. This update ensures that only unacknowledged alerts are considered in the `get_alert_stats` and `get_alert_count_by_message` methods. Adding `acknowledgement=None` filters improves data accuracy and consistency.

--- humitifier-server/src/main/views.py
+++ humitifier-server/src/main/views.py
@@ -224,11 +224,13 @@ class DashboardView(LoginRequiredMixin, FilteredListView):
     def get_alert_stats(self):
         num_critical = Host.objects.filter(
             alerts__severity=AlertSeverity.CRITICAL,
+            alerts__acknowledgement=None,
             archived=False,
         ).count()
         num_warning = (
             Host.objects.filter(
                 alerts__severity=AlertSeverity.WARNING,
+                alerts__acknowledgement=None,
                 archived=False,
             )
             .exclude(
@@ -239,6 +241,7 @@ class DashboardView(LoginRequiredMixin, FilteredListView):
         num_info = (
             Host.objects.filter(
                 alerts__severity=AlertSeverity.INFO,
+                alerts__acknowledgement=None,
                 archived=False,
             )
             .exclude(
@@ -253,6 +256,7 @@ class DashboardView(LoginRequiredMixin, FilteredListView):
     def get_alert_count_by_message(self):
         return (
             Alert.objects.get_for_user(self.request.user)
+            .filter(acknowledgement=None)
             .values("short_message")
             .annotate(count=Count("id"))
         )
