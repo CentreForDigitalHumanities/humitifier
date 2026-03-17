a60c96359394c228243e257952a00d2b88c87488
Mees, T.D. (Ty) <t.d.mees@uu.nl>
2026-03-17 15:35:55 +0100

    chore: bump version to 4.14.0
    Updated version references across all modules to 4.14.0.

--- humitifier-server/src/humitifier_server/settings.py
+++ humitifier-server/src/humitifier_server/settings.py
@@ -24,8 +24,8 @@ from . import env
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-HUMITIFIER_VERSION = "4.13.0"
-HUMITIFIER_VERSION_NAME = "Skynet Edition"
+HUMITIFIER_VERSION = "4.14.0"
+HUMITIFIER_VERSION_NAME = ".. is not a monitoring solution, honest"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
