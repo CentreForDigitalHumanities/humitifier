16a86426f3b1e4ba8d4f5064fcd174e81d08f406
Mees, T.D. (Ty) <t.d.mees@uu.nl>
2026-09-07 08:38:10 +0200

    chore: bump version numbers to 5.0.1
    Updated version numbers across all components to 5.0.1, including settings, constants, and project configuration files. This ensures consistency and reflects the latest changes in the project.

--- humitifier-scanner/src/humitifier_scanner/constants.py
+++ humitifier-scanner/src/humitifier_scanner/constants.py
@@ -1,4 +1,4 @@
-HUMITIFIER_VERSION = "5.0.0"
+HUMITIFIER_VERSION = "5.0.1"
 
 DEB_OS_LIST = ["debian", "ubuntu"]
 RPM_OS_LIST = ["redhat", "red hat", "centos", "fedora", "almalinux"]
--- humitifier-server/src/humitifier_server/settings.py
+++ humitifier-server/src/humitifier_server/settings.py
@@ -24,7 +24,7 @@ from .config import CONFIG as HUMITIFIER_CONFIG
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-HUMITIFIER_VERSION = "5.0.0"
+HUMITIFIER_VERSION = "5.0.1"
 HUMITIFIER_VERSION_NAME = "Sentient Spaghetti"
 
 # Quick-start development settings - unsuitable for production
